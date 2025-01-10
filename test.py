2b26489c3367f0e038b1b8652a8939cd74193561
rxsweet <105535100+rxsweet@users.noreply.github.com>
2025-01-10 09:41:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ from datetime import datetime   #时间
 
 
 def getContent(url):#获取网站的内容，将获取的内容返回
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3314.0 Safari/537.36 SE 2.X MetaSr 1.0'}
     try:
-        r=requests.get(url)
+        r=requests.get(url,headers=headers)
         print(r)
         if r.status_code==200:
             r.encoding='utf-8'    #编码方式
@@ -25,5 +26,4 @@ def getContent(url):#获取网站的内容，将获取的内容返回
 
 
 if "__name__==__main__":#主程序开始的地方
-    #getContent("https://www.85la.com/internet-access/free-network-nodes")
-    getContent("https://www.85la.com")
+    getContent("https://www.85la.com/")
