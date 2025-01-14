feecdf382bc251b555f524a20f4924278be0ef05
rxsweet <105535100+rxsweet@users.noreply.github.com>
2025-01-14 17:49:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import requests
 PATH = os.path.abspath(os.path.dirname(__file__))  # 1.os.path.dirname(_file_) 返回脚本的绝对路径.. 2.可嵌套使用，如 os.path.dirname(os.path.dirname(path) ) 返回父路径的父路径,,https://blog.csdn.net/qq_43404784/article/details/88994350
 print(PATH)
 #http://localhost:25500/sub
-res=requests.get('http://localhost:25500/subsub?&url=https%3A%2F%2Frxsub.eu.org%2Fsub%2Frx.yaml&target=mixed&emoji=true&append_type=true&append_info=true&scv=false&udp=false&list=false&sort=false&fdn=false&insert=false',timeout=5)#设置5秒超时防止卡死
+res=requests.get('http://127.0.0.1:25500/subsub?&url=https%3A%2F%2Frxsub.eu.org%2Fsub%2Frx.yaml&target=mixed&emoji=true&append_type=true&append_info=true&scv=false&udp=false&list=false&sort=false&fdn=false&insert=false',timeout=5)#设置5秒超时防止卡死
 print(res)
 print('------------------------------------------------')
 print(res.text)
