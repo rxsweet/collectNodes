85c57053203f7b6828adbb8271b0e9247318d51f
rxsweet <105535100+rxsweet@users.noreply.github.com>
2025-01-16 12:06:13 +0800

    Update subconvert.py

--- subconvert.py
+++ subconvert.py
@@ -6,10 +6,94 @@ import requests
 import urllib.parse
 from datetime import datetime
 import time
-#clash文件地址
+import yaml
+
 
 #默认转clash配置文件.ini地址
 INI_CONFIG = 'https://raw.githubusercontent.com/rxsweet/all/main/githubTools/clashConfig.ini'
+
+
+def proxies_rm(proxies_list):
+    # 去重复，重名，空名，float型password
+    raw_length = len(proxies_list)
+    length = len(proxies_list)
+    begin = 0
+    rm = 0
+    name_none = 0
+    passErr = 0
+    print(f'proxies: 列表去重')
+    while begin < length:
+        if (begin + 1) == 1:
+            print(f'\n-----去重开始-----\n起始数量{length}')
+        elif (begin + 1) % 100 == 0:
+            print(f'当前基准{begin + 1}-----当前数量{length}')
+        elif (begin + 1) == length and (begin + 1) % 100 != 0:
+            repetition = raw_length - length - passErr
+            print(f'当前基准{begin + 1}-----当前数量{length}\n--------\n重复数量{repetition}\n重名数量{rm}\n空名数{name_none}\nfloat型password数量{passErr}\n-----去重完成-----\n')
+        if proxies_list[begin]['name'] == None or proxies_list[begin]['name'] == '' or proxies_list[begin]['name'] == ' ':
+            proxies_list[begin]['name'] = 'name-None' + '-' + proxies_list[begin]['type'] + '+' +  str(name_none)
+            name_none += 1
+        begin_2 = begin + 1
+        name_same = 0
+        while begin_2 <= (length - 1):
+            if proxies_list[begin]['server'] == proxies_list[begin_2]['server'] and proxies_list[begin]['port'] == proxies_list[begin_2]['port']:
+                if 'password' in proxies_list[begin] and 'password' in proxies_list[begin_2]:
+                    if proxies_list[begin]['password'] == proxies_list[begin_2]['password']:
+                        proxies_list.pop(begin_2)
+                        length -= 1
+                        begin_2 -= 1
+                if 'uuid' in proxies_list[begin] and 'uuid' in proxies_list[begin_2]:
+                    if proxies_list[begin]['uuid'] == proxies_list[begin_2]['uuid']:
+                        proxies_list.pop(begin_2)
+                        length -= 1
+                        begin_2 -= 1
+            else:
+                if proxies_list[begin]['name'] == proxies_list[begin_2]['name']:
+                    name_same += 1
+                    proxies_list[begin_2]['name'] = str(proxies_list[begin_2]['name']) + '+' + str(name_same)
+            begin_2 += 1
+        #if name_same > 0:
+            #print(f"{proxies_list[begin]['name']} 重名数量：{name_same}")
+        rm += name_same
+        #删除空密码和password为float型的节点
+        if proxies_list[begin]['type'] == 'ss' or proxies_list[begin]['type'] == 'trojan' or proxies_list[begin]['type'] == 'ssr':  
+            try:
+                if proxies_list[begin]['password'] == None or proxies_list[begin]['password'] == '':    #空密码
+                    print(f"{proxies_list[begin]['name']}的password为空！")
+                    #print(proxies_list[begin])
+                    proxies_list.pop(begin)
+                    length -= 1
+                    begin -= 1
+                else:
+                    try:  # 如果能运行float(s)语句，返回True（字符串s是浮点数）
+                        float(proxies_list[begin]['password'])
+                        #print(f"{proxies_list[begin]['name']}的password为float型！")
+                        proxies_list.pop(begin)
+                        length -= 1
+                        begin -= 1
+                    except ValueError:  # ValueError为Python的一种标准异常，表示"传入无效的参数"
+                        pass
+            except:
+                proxies_list.pop(begin)
+                length -= 1
+                begin -= 1
+        begin += 1 
+    print(f'去重后剩余总数:{len(proxies_list)}')
+    return proxies_list
+    
+def YAML_rm(source):
+    #读取yaml文件
+    with open(source, 'r',encoding = 'utf-8') as f:
+        try:
+            proxyconfig = yaml.load(f.read(), Loader=yaml.FullLoader)
+        except Exception as err:
+            print(f"读取{source}文件失败")
+    #去重
+    proxyconfig['proxies'] = proxies_rm(proxyconfig['proxies'])
+    #写入
+    with open(source, 'w',encoding = 'utf-8') as file:
+        #file = yaml.dump(proxyconfig, file,default_flow_style=False, sort_keys=False, allow_unicode=True, width=750, indent=2)
+        file = yaml.dump(proxyconfig, file, allow_unicode=True, indent=2)
 
 # 注意 订阅地址必须是base64,或者yaml，(直接是url节点内容的话，会解析错误)
 def convert_remote(url='', output_type='clash',configUrl = INI_CONFIG):
@@ -71,7 +155,7 @@ def convert_remote(url='', output_type='clash',configUrl = INI_CONFIG):
     return sub_content
 
 #源文件转到目标文件，类型为output_type，共4个类型：clash,base64,url,YAML
-def fileToFile(source,output_type,output):
+def sub_convert(source,output_type,output):
 
     time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     print(f'[{time}]: subconvert [{output_type}]- [{source}] to [{output}]')
@@ -87,7 +171,7 @@ def subconverter_install():
         #os.system()
         if not os.path.exists('./subconverter.tar.gz'):
             os.system("wget -O subconverter.tar.gz https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz")
-            #os.system("wget -O subconverter.tar.gz https://github.com/lonelam/subconverter/releases/latest/download/subconverter_linux64.tar.gz")
+            #os.system("wget -O subconverter.tar.gz https://github.com/lonelam/subconverter/releases/latest/download/subconverter_linux64.tar.gz")    #支持hy2
             os.system("tar -zxf subconverter.tar.gz -C ./")
             os.system("chmod +x ./subconverter/subconverter && nohup ./subconverter/subconverter >./subconverter.log 2>&1 &")
             print('=subconverter已经启动=')
@@ -103,15 +187,23 @@ def subconverter_install():
 
 if __name__=='__main__':
     #获取参数携带的参数
+
     import sys
     args = sys.argv
     #print(args)
-    #下载安装subconverter
-    subconverter_install()
-    time.sleep(3)
-    if args:
+
+    if len(args) == 4:
+        #下载安装subconverter
+        subconverter_install()
+        time.sleep(3)
         #args[0]是py文件名
         source = args[1]
         output_type = args[2]
         output = args[3]
-        fileToFile(source,output_type,output)
+        sub_convert(source,output_type,output)
+    if len(args) == 3:
+        source = args[1]
+        act = args[2]
+        if act == 'rm':
+            YAML_rm(source)
+    
